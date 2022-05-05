288f343fdb0e3024cccc4c87e16ac77562755550
Koech-code <nixonkipkorir01@gmail.com>
2022-05-05 18:36:50 +0300

    change class name

--- battery/nubbin_battery.py
+++ battery/nubbin_battery.py
@@ -3,7 +3,7 @@ from abc import ABC
 from car import Car
 
 
-class SpindlerBattery(Car, ABC):
+class nubbinBattery(Car, ABC):
     def __init__(self, last_service_date, current_date):
         super().__init__(last_service_date)
         self.current_date = current_date
