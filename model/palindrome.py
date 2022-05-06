fcef5cfd4fd56ecc1ec151b55fe7a32fef55b1d3
Koech-code <nixonkipkorir01@gmail.com>
2022-05-06 12:16:38 +0300

    change class name from calliope to palindrome

--- model/palindrome.py
+++ model/palindrome.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from engine.sternman_engine import SternmanEngine
 from battery.spindler_battery import SpindlerBattery
 
-class Calliope(SternmanEngine, SpindlerBattery):
+class palindrome(SternmanEngine, SpindlerBattery):
     def needs_service(self):
         if self.engine_should_be_serviced() or self.battery_should_be_serviced():
             return True
