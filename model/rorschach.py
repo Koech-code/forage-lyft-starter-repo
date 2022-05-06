0bfdfc0512c6b261a22243b3e9d75a913954480c
Koech-code <nixonkipkorir01@gmail.com>
2022-05-06 12:17:41 +0300

    change class name from calliope to rorschach

--- model/rorschach.py
+++ model/rorschach.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from engine.willoughby_engine import WilloughbyEngine
 from battery.nubbin_battery import nubbinBattery
 
-class Calliope(WilloughbyEngine, nubbinBattery):
+class rorschach(WilloughbyEngine, nubbinBattery):
     def needs_service(self):
         if self.engine_should_be_serviced() or self.battery_should_be_serviced():
             return True
