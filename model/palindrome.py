ae44fe6e2f469e9533444302226ac59acee6ae39
Koech-code <nixonkipkorir01@gmail.com>
2022-05-06 12:00:04 +0300

    import engine and battery classes with their methods

--- model/palindrome.py
+++ model/palindrome.py
@@ -1,12 +1,11 @@
 from datetime import datetime
 
 from engine.sternman_engine import SternmanEngine
+from battery.spindler_battery import SpindlerBattery
 
-
-class Palindrome(SternmanEngine):
+class Calliope(SternmanEngine, SpindlerBattery):
     def needs_service(self):
-        service_threshold_date = self.last_service_date.replace(year=self.last_service_date.year + 4)
-        if service_threshold_date < datetime.today().date() or self.engine_should_be_serviced():
+        if self.engine_should_be_serviced() or self.battery_should_be_serviced():
             return True
         else:
             return False
