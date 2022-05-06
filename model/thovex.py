7099cd9d2773dfe2337d5a94e1c8925bf1391aa9
Koech-code <nixonkipkorir01@gmail.com>
2022-05-06 12:18:27 +0300

    change class name from calliope to thovex

--- model/thovex.py
+++ model/thovex.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from engine.capulet_engine import CapuletEngine
 from battery.nubbin_battery import nubbinBattery
 
-class Calliope(CapuletEngine, nubbinBattery):
+class thovex(CapuletEngine, nubbinBattery):
     def needs_service(self):
         if self.engine_should_be_serviced() or self.battery_should_be_serviced():
             return True
